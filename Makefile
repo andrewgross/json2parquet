68e466147c078bfbffc306d8e4462ca33bf65eb7
Andrew Gross <andrew.w.gross@gmail.com>
2017-09-13 16:10:27 -0400

    Update travis, setup and Makefile in preparation for PyPi release

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
-setup:
-	@pip install -r development.txt
-	@pre-commit install
+PACKAGE=json2parquet
+CUSTOM_PIP_INDEX=pypi
 
-pre_commit:
+all: setup test
+
+prepare: clean install_deps
+
+setup: prepare
+
+pre_commit: setup
 	@pre-commit run --all-files
 
 test:
@@ -10,5 +15,49 @@ test:
 
 clean:
 	@rm -rf .cache
-	@rm -rf tests/__pycache__
 	@rm -rf build/
+	@echo "Removing garbage..."
+	@find . -name '*.pyc' -delete
+	@find . -name '*.so' -delete
+	@find . -name __pycache__ -delete
+	@rm -rf .coverage *.egg-info *.log build dist MANIFEST yc
+
+install_deps:
+	@if [ -z $$VIRTUAL_ENV ]; then \
+		echo "===================================================="; \
+		echo "You're not running this from a virtualenv, wtf?"; \
+		echo "ಠ_ಠ"; \
+		echo "===================================================="; \
+		exit 1; \
+	fi
+
+	@if [ -z $$SKIP_DEPS ]; then \
+		echo "Installing missing dependencies..."; \
+		[ -e development.txt  ] && pip install --quiet -r development.txt; \
+	fi
+	@pre-commit install
+	@python setup.py develop &> .build.log
+
+publish: clean tag
+	@if [ -e "$$HOME/.pypirc" ]; then \
+		echo "Uploading to '$(CUSTOM_PIP_INDEX)'"; \
+		python setup.py register -r "$(CUSTOM_PIP_INDEX)"; \
+		python setup.py sdist upload -r "$(CUSTOM_PIP_INDEX)"; \
+	else \
+		echo "You should create a file called '.pypirc' under your home dir.\n"; \
+		echo "That's the right place to configure 'pypi' repos.\n"; \
+		exit 1; \
+	fi
+
+tag:
+	@if [ $$(git rev-list $$(git describe --abbrev=0 --tags)..HEAD --count) -gt 0 ]; then \
+		if [ $$(git log  -n 1 --oneline $$(git describe --abbrev=0 --tags)..HEAD CHANGELOG.rst | wc -l) -gt 0 ]; then \
+			git tag $$(python setup.py --version) && git push --tags || echo 'Version already released, update your version!'; \
+		else \
+			echo "CHANGELOG not updated since last release!"; \
+			exit 1; \
+		fi; \
+	else \
+		echo "No commits since last release!"; \
+		exit 1;\
+	fi
--- setup.py
+++ setup.py
@@ -1,57 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import re
-import os
+
 from setuptools import setup, find_packages
 
+with open('json2parquet/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
 
-def parse_requirements():
-    """Rudimentary parser for the `requirements.txt` file
-    We just want to separate regular packages from links to pass them to the
-    `install_requires` and `dependency_links` params of the `setup()`
-    function properly.
-    """
-    try:
-        requirements = \
-            map(str.strip, local_file('requirements.txt').splitlines())
-    except IOError:
-        raise RuntimeError("Couldn't find the `requirements.txt' file :(")
-
-    links = []
-    pkgs = []
-    for req in requirements:
-        if not req:
-            continue
-        if 'http:' in req or 'https:' in req:
-            links.append(req)
-            name, version = re.findall("\#egg=([^\-]+)-(.+$)", req)[0]
-            pkgs.append('{0}=={1}'.format(name, version))
-        else:
-            pkgs.append(req)
-
-    return pkgs, links
+if not version:
+    raise RuntimeError('Cannot find version information')
 
 
-def local_file(f):
-    return open(os.path.join(os.path.dirname(__file__), f)).read()
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
-
-install_requires, dependency_links = parse_requirements()
-
-if __name__ == '__main__':
-
-    setup(
-        name="json2parquet",
-        version='0.0.1',
-        description="json2parquet",
-        long_description=local_file('README.md'),
-        author='Andrew Gross',
-        author_email='andrew.w.gross@gmail.com',
-        url='https://github.com/andrewgross/json2parquet',
-        packages=find_packages(exclude=['*tests*']),
-        install_requires=install_requires,
-        include_package_data=True,
-        dependency_links=dependency_links,
-        classifiers=[
-            'Programming Language :: Python',
-        ],
-        zip_safe=False,
-    )
+setup(
+    name='json2parquet',
+    version=version,
+    description='A simple Parquet converter for JSON/python data',
+    long_description=readme,
+    author='Andrew Gross',
+    author_email='andrew.w.gross@gmail',
+    url='https://github.com/andrewgross/json2parquet',
+    install_requires=[
+        'pyarrow>=0.6.0'
+    ],
+    packages=[n for n in find_packages() if not n.startswith('tests')],
+    include_package_data=True,
+)
